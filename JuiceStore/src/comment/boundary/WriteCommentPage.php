4f27ade3cdce7a187c82e67b8ddc956c572813d9
beryildiz <berkan.mehmet.yildiz@gmail.com>
2023-03-09 15:21:42 +0100

    Comment section added

---
+++ JuiceStore/src/comment/boundary/WriteCommentPage.php
@@ -0,0 +1,10 @@
+
+
+<form method="post" action="item?id=<?php echo $item->id; ?>">
+    <label>
+<textarea name="content" class="form-control">
+</textarea>
+    </label>
+    <br>
+    <input type="submit" value="Add comment" class="btn btn-primary"/>
+</form>
